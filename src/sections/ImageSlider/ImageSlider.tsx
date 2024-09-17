922031f6367d49587239d4953dde5f2cb03d0b1f
Damlil <112380867+Damlil@users.noreply.github.com>
2024-09-17 11:20:19 +0200

    d

--- src/sections/ImageSlider/ImageSlider.tsx
+++ src/sections/ImageSlider/ImageSlider.tsx
@@ -1,73 +1,73 @@
-// import { useState } from "react"
-// import { ArrowBigLeft, ArrowBigRight, CircleDot, Circle } from 'lucide-react'
-// import React from "react"
-// import '../ImageSlider/image-slider.css'
+import { useState } from "react"
+import { ArrowBigLeft, ArrowBigRight, CircleDot, Circle } from 'lucide-react'
+import React from "react"
+import '../ImageSlider/image-slider.css'
 
 
-// type ImageSliderProps = {
-//     imageUrls: string[]
-// }
+type ImageSliderProps = {
+    imageUrls: string[]
+}
 
-// export function ImageSlider({ imageUrls }: ImageSliderProps) {
-//     const [imageIndex, setImageIndex] = useState(0)
+export function ImageSlider({ imageUrls }: ImageSliderProps) {
+    const [imageIndex, setImageIndex] = useState(0)
 
-//     function showNextImage() {
+    function showNextImage() {
 
-//         setImageIndex(index => {
-//             if (index === imageUrls.length - 1) return 0
-//             return index +1
-//         })
-//     }
+        setImageIndex(index => {
+            if (index === imageUrls.length - 1) return 0
+            return index +1
+        })
+    }
 
-//     function showPrevImage() {
+    function showPrevImage() {
 
-//         setImageIndex(index => {
-//             if (index === 0) return imageUrls.length - 1
-//             return index - 1
-//         })
-//     }
+        setImageIndex(index => {
+            if (index === 0) return imageUrls.length - 1
+            return index - 1
+        })
+    }
 
 
-//     return  <div style={{
-//         width: '100%',
-//         height: '100%',
-//         position: 'relative',
-//     }}>
+    return  <div style={{
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+    }}>
 
-//     <div style={{
-//         width: '100%',
-//         height: '100%',
-//         display: 'flex',
-//         overflow: 'hidden'
-//     }}>
-//         {imageUrls.map(url => (
-//                 <img key={url} src={url} className="img-slider-img" 
-//                 style={{ translate: `${-100 * imageIndex}%` }} />
-//         ))}
+    <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflow: 'hidden'
+    }}>
+        {imageUrls.map(url => (
+                <img key={url} src={url} className="img-slider-img" 
+                style={{ translate: `${-100 * imageIndex}%` }} />
+        ))}
     
-//     </div>
+    </div>
 
-//         <button onClick={showPrevImage} className="img-slider-btn" style= {{ left: 0}}><ArrowBigLeft/></button>
-//         <button onClick={showNextImage} className="img-slider-btn" style= {{ right: 0}}><ArrowBigRight/></button>
+        <button onClick={showPrevImage} className="img-slider-btn" style= {{ left: 0}}><ArrowBigLeft/></button>
+        <button onClick={showNextImage} className="img-slider-btn" style= {{ right: 0}}><ArrowBigRight/></button>
         
 
-//         <div style= {{
-//             position: 'absolute',
-//             bottom: '.5rem',
-//             left: '50%',
-//             translate: '-50%',
-//             display: 'flex',
-//             gap: '.25rem'
+        <div style= {{
+            position: 'absolute',
+            bottom: '.5rem',
+            left: '50%',
+            translate: '-50%',
+            display: 'flex',
+            gap: '.25rem'
 
-//         }}>
-//             {imageUrls.map((_, index) => (
-//                 <button key={index} className="img-slider-dot-btn" onClick={() => setImageIndex(index)}>
-//                     {index === imageIndex ? <CircleDot /> : <Circle />}</button>
-//             ))}
-//         </div>
+        }}>
+            {imageUrls.map((_, index) => (
+                <button key={index} className="img-slider-dot-btn" onClick={() => setImageIndex(index)}>
+                    {index === imageIndex ? <CircleDot /> : <Circle />}</button>
+            ))}
+        </div>
 
 
-//     </div>
-// }
+    </div>
+}
 
 
