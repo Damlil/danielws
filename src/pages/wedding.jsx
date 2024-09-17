86b440ac3f5f8cdb3f62ad85ea1c80a02727b7d5
Damlil <112380867+Damlil@users.noreply.github.com>
2024-09-17 11:18:57 +0200

    OUT

--- src/pages/wedding.jsx
+++ src/pages/wedding.jsx
@@ -1,27 +1,27 @@
-import styles from './PagesStyles.module.css'
-import { ImageSlider } from '../sections/ImageSlider/ImageSlider'
+// import styles from './PagesStyles.module.css'
+// import { ImageSlider } from '../sections/ImageSlider/ImageSlider'
 
-import pic1 from '../assets/photos/bali.jpg'
-import pic2 from '../assets/photos/london.jpg'
-import pic3 from '../assets/photos/mom&mika.jpg'
-import pic4 from '../assets/photos/wedding.jpg'
+// import pic1 from '../assets/photos/bali.jpg'
+// import pic2 from '../assets/photos/london.jpg'
+// import pic3 from '../assets/photos/mom&mika.jpg'
+// import pic4 from '../assets/photos/wedding.jpg'
 
-const IMAGES = [pic1, pic2, pic3, pic4]
+// const IMAGES = [pic1, pic2, pic3, pic4]
 
 
 
-export default function Gallery() {
-  return (
-    <div
-    style={{
-      maxWidth: '1200px',
-      width: '100%',
-      margin: '0 auto',
+// export default function Gallery() {
+//   return (
+//     <div
+//     style={{
+//       maxWidth: '1200px',
+//       width: '100%',
+//       margin: '0 auto',
 
-    }}>
+//     }}>
 
-    <ImageSlider imageUrls={IMAGES} />
+//     <ImageSlider imageUrls={IMAGES} />
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
