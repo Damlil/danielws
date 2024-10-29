49de4c41c0e50cde678c667f789144d500030551
Damlil <112380867+Damlil@users.noreply.github.com>
2024-10-29 17:48:51 +0100

    s
    s

--- src/pages/indonesia.jsx
+++ src/pages/indonesia.jsx
@@ -16,16 +16,11 @@ import pic12 from '../assets/photos/indonesia/bali12.jpg'
 import pic13 from '../assets/photos/indonesia/bali13.jpg'
 import pic14 from '../assets/photos/indonesia/bali14.jpg'
 import pic15 from '../assets/photos/indonesia/bali15.jpg'
-import pic16 from '../assets/photos/indonesia/bali16.jpg'
-import pic17 from '../assets/photos/indonesia/bali17.jpg'
-import pic18 from '../assets/photos/indonesia/bali18.jpg'
-import pic19 from '../assets/photos/indonesia/bali19.jpg'
-import pic20 from '../assets/photos/indonesia/bali20.jpg'
 
 
 
 const IMAGES = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11,pic12,pic13,pic14,
-  pic15,pic16,pic17,pic18,pic19,pic20]
+  pic15]
 
 
 
--- src/pages/wedding.jsx
+++ src/pages/wedding.jsx
@@ -16,52 +16,11 @@ import pic12 from '../assets/photos/wedding/wed12.jpg'
 import pic13 from '../assets/photos/wedding/wed13.jpg'
 import pic14 from '../assets/photos/wedding/wed14.jpg'
 import pic15 from '../assets/photos/wedding/wed15.jpg'
-import pic16 from '../assets/photos/wedding/wed16.jpg'
-import pic17 from '../assets/photos/wedding/wed17.jpg'
-import pic18 from '../assets/photos/wedding/wed18.jpg'
-import pic19 from '../assets/photos/wedding/wed19.jpg'
-import pic20 from '../assets/photos/wedding/wed20.jpg'
-import pic21 from '../assets/photos/wedding/wed21.jpg'
-import pic22 from '../assets/photos/wedding/wed22.jpg'
-import pic23 from '../assets/photos/wedding/wed23.jpg'
-import pic24 from '../assets/photos/wedding/wed24.jpg'
-import pic25 from '../assets/photos/wedding/wed25.jpg'
-import pic26 from '../assets/photos/wedding/wed26.jpg'
-import pic27 from '../assets/photos/wedding/wed27.jpg'
-import pic28 from '../assets/photos/wedding/wed28.jpg'
-import pic29 from '../assets/photos/wedding/wed29.jpg'
-import pic30 from '../assets/photos/wedding/wed30.jpg'
-import pic31 from '../assets/photos/wedding/wed31.jpg'
-import pic32 from '../assets/photos/wedding/wed32.jpg'
-import pic33 from '../assets/photos/wedding/wed33.jpg'
-import pic34 from '../assets/photos/wedding/wed34.jpg'
-import pic35 from '../assets/photos/wedding/wed35.jpg'
-import pic36 from '../assets/photos/wedding/wed36.jpg'
-import pic37 from '../assets/photos/wedding/wed37.jpg'
-import pic38 from '../assets/photos/wedding/wed38.jpg'
-import pic39 from '../assets/photos/wedding/wed39.jpg'
-import pic40 from '../assets/photos/wedding/wed40.jpg'
-import pic41 from '../assets/photos/wedding/wed41.jpg'
-import pic42 from '../assets/photos/wedding/wed42.jpg'
-import pic43 from '../assets/photos/wedding/wed43.jpg'
-import pic44 from '../assets/photos/wedding/wed44.jpg'
-import pic45 from '../assets/photos/wedding/wed45.jpg'
-import pic46 from '../assets/photos/wedding/wed46.jpg'
-import pic47 from '../assets/photos/wedding/wed47.jpg'
-import pic48 from '../assets/photos/wedding/wed48.jpg'
-import pic49 from '../assets/photos/wedding/wed49.jpg'
-import pic50 from '../assets/photos/wedding/wed50.jpg'
-import pic51 from '../assets/photos/wedding/wed51.jpg'
-import pic52 from '../assets/photos/wedding/wed52.jpg'
-import pic53 from '../assets/photos/wedding/wed53.jpg'
-import pic54 from '../assets/photos/wedding/wed54.jpg'
 
 
 
 const IMAGES = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11,pic12,pic13,pic14,
-  pic15,pic16,pic17,pic18,pic19,pic20,pic21,pic22,pic23,pic24,pic25,pic26,pic27,pic28,pic29,pic30,pic31,pic32,pic33,pic34,pic35,pic36,pic37,pic38,pic39,pic40,pic41,
-  pic42,pic43,pic44,pic45,pic46,pic47,pic48,pic49,pic50,pic51,pic52,pic53,pic54
- ]
+  pic15]
 
 
 
