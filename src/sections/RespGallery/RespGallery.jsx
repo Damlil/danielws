4d14f8eb962b87d43c124dccfd4906bcae98f18b
Damlil <112380867+Damlil@users.noreply.github.com>
2025-01-06 15:45:13 -0600

    s
    s

--- src/sections/RespGallery/RespGallery.jsx
+++ src/sections/RespGallery/RespGallery.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 import pic1 from '../RespGallery/photos/coffeeBar/coffeeBar1.jpg'
 import pic2 from '../RespGallery/photos/coffeeBar/coffeeBar2.jpg'
 import pic3 from '../RespGallery/photos/coffeeBar/coffeeBar3.jpg'
+import pic4 from '../RespGallery/photos/coffeeBar/coffeeBar4.jpg'
+import pic5 from '../RespGallery/photos/coffeeBar/coffeeBar5.jpg'
+import pic6 from '../RespGallery/photos/coffeeBar/coffeeBar6.jpg'
+import pic7 from '../RespGallery/photos/coffeeBar/coffeeBar7.jpg'
+import pic8 from '../RespGallery/photos/coffeeBar/coffeeBar8.jpg'
+import pic9 from '../RespGallery/photos/coffeeBar/coffeeBar9.jpg'
 import '../RespGallery/imageGallery.css'
 
 
@@ -49,10 +55,24 @@ export default function RespGallery() {
             <img src={pic1} alt="" onClick={() => handleImageClick(pic1)} />
             </div>
             <div className='photo'>
-            <img src={pic1} alt="" />
+            <img src={pic2} alt="" onClick={() => handleImageClick(pic2)} />
             </div>
             <div className='photo'>
-            <img src={pic1} alt="" />
+            <img src={pic3} alt="" onClick={() => handleImageClick(pic3)} />
+            </div>
+         </div>
+
+
+         <div className='column2'>
+
+            <div className='photo'>
+            <img src={pic4} alt="" onClick={() => handleImageClick(pic4)} />
+            </div>
+            <div className='photo'>
+            <img src={pic5} alt="" onClick={() => handleImageClick(pic5)} />
+            </div>
+            <div className='photo'>
+            <img src={pic6} alt="" onClick={() => handleImageClick(pic6)} />
             </div>
          </div>
 
@@ -60,27 +80,13 @@ export default function RespGallery() {
          <div className='column'>
 
             <div className='photo'>
-            <img src={pic2} alt="" />
+            <img src={pic7} alt="" onClick={() => handleImageClick(pic7)} />
             </div>
             <div className='photo'>
-            <img src={pic2} alt="" />
+            <img src={pic8} alt="" onClick={() => handleImageClick(pic8)} />
             </div>
             <div className='photo'>
-            <img src={pic2} alt="" />
-            </div>
-         </div>
-
-
-         <div className='column'>
-
-            <div className='photo'>
-            <img src={pic3} alt="" />
-            </div>
-            <div className='photo'>
-            <img src={pic3} alt="" />
-            </div>
-            <div className='photo'>
-            <img src={pic3} alt="" />
+            <img src={pic9} alt="" onClick={() => handleImageClick(pic9)} />
             </div>
          </div>
         
