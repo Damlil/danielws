22b839e53e0648d8f604438123e27d086bfbaffc
Damlil <112380867+Damlil@users.noreply.github.com>
2025-01-07 19:07:16 -0600

    s
    s

--- src/sections/RespGallery/AlbumCoffeFood.jsx
+++ src/sections/RespGallery/AlbumCoffeFood.jsx
@@ -29,9 +29,13 @@ import pic21 from '../RespGallery/photos/coffeeBar/coffeeBar21.jpg'
 
 // Array of photo imports for dynamic rendering
 const photos = [
-  pic1, pic2, pic3, pic4, pic5, pic6,
-  pic7, pic8, pic9, pic10, pic11,pic12,pic13,pic14,pic15,pic16,
-pic17,pic18,pic19,pic20,pic21];
+     pic2, pic3,
+    pic4, pic5, pic6,
+    pic7, pic8, pic9,
+    pic10, pic11,pic12,
+    pic13,pic14,pic15,
+    pic16,pic17,pic18,
+    pic19,pic20,pic21,pic1];
 
 export default function RespGallery() {
   // Function to navigate back to the previous page
